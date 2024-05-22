95b8e523bbe0c98a42c373b7acf1bff9e8ac8d44
miiamole <miia_pelaez@hotmail.com>
2024-05-22 17:22:07 +0200

    form

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -31,35 +31,37 @@ function handleSubmitForm(e) {
 
   return (
     <>
-      <form onSubmit={handleSubmitForm}>
-        <input
-          type="text"
-          placeholder="first name"
-          value={firstName}
-          onChange={handleFirstNameChange}
-        />
-        <input
-          type="text"
-          placeholder="last name"
-          value={lastName}
-          onChange={handleLastNameChange}
-        />
-        <input
-          type="text"
-          placeholder="street name and number"
-          value={adress}
-          onChange={handleAdressChange}
-        />
-        <input
-          type="text"
-          placeholder="city"
-          value={city}
-          onChange={handleCityChange}
-        />
-        <button type="submit">Submit</button>
+      <form onSubmit={handleSubmitForm} className="form-container">
+        <div className="user-form">
+          <input
+            type="text"
+            placeholder="first name"
+            value={firstName}
+            onChange={handleFirstNameChange}
+          />
+          <input
+            type="text"
+            placeholder="last name"
+            value={lastName}
+            onChange={handleLastNameChange}
+          />
+          <input
+            type="text"
+            placeholder="street name and number"
+            value={adress}
+            onChange={handleAdressChange}
+          />
+          <input
+            type="text"
+            placeholder="city"
+            value={city}
+            onChange={handleCityChange}
+          />
+          <button type="submit">Submit</button>
+        </div>
       </form>
 
-      <h2>Chhose payment method:</h2>
+      <h2>Choose payment method:</h2>
       <button>Card</button>
       <button>Swish</button>
     </>
