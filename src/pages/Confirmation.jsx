682e7917f0920fc7d53a13ee3a64124ccecc712d
miiamole <miia_pelaez@hotmail.com>
2024-05-29 21:40:40 +0200

    fixat med confirmation page

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -5,13 +5,14 @@ function Confirmation() {
   const [order, setOrder] = useState([]);
   const [deliveryTime, setDeliveryTime] = useState(null);
   const { orderId } = useParams();
-
+  const [customer, setCustomer] = useState({});
   useEffect(() => {
     fetch(`http://localhost:3000/orders/${orderId}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setOrder(data.items);
+        setCustomer(data.customer);
       });
 
     //ville ha ett heltal mellan 10 och 45.
@@ -19,9 +20,17 @@ function Confirmation() {
     setDeliveryTime(randomTime);
   }, [orderId]);
 
+const capitalizeFirstLetter = (string) => {
+  if (!string) return "";
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+
+
   return (
     <>
-      <h3>Thank you for your order</h3>
+      <h3>
+        Thank you for your order {capitalizeFirstLetter(customer.firstName)}
+      </h3>
       <p>it is now beeing prepared</p>
       <p>Estimated time to delivery: {deliveryTime} minutes</p>
 
@@ -35,7 +44,7 @@ function Confirmation() {
           order.map((item) => (
             <div key={item.id} className="menu-card">
               <img src={item.image} className="menu-image" />
-              <h3>{item.title}</h3>
+              <h3>{item.name}</h3>
             </div>
           ))}
       </div>
