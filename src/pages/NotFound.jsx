c17308d551da14e0e928dce0dbcb3c1e237186aa
miiamole <miia_pelaez@hotmail.com>
2024-06-03 21:09:13 +0200

    färdig med not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 function NotFound() {
-    return (
-      <>
-        <h1 className="clock-container">
-          Hello, is it me you're looking for? 🎵 Sorry, couldn't find what
-          you're after here. But hey, while you're here, why not take a moment
-          to enjoy the silence?
-        </h1>
-      </>
-    );
+  return (
+    <>
+      <div className="color-wrapper">
+        <div className="not-found-container">
+          <h1>Sorry, couldn't find what you're after here.</h1>
+          <h2>
+            But hey, while you're here, why not take a moment to enjoy the
+            silence?
+          </h2>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default NotFound;
